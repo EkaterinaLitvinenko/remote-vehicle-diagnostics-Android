905b967c021849b3559031767cdb515b2b469276
ekaterinalitvinenko <tim.ekaterina96@gmail.com>
2024-03-12 22:58:39 +0100

    Init

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Vzdialena Diagnostika Vozidiel"
+include(":app")
